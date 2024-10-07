b99dcb80dd013eba9200f7a024843873270736ec
devscirpter <148705534+devscirpter@users.noreply.github.com>
2024-10-06 21:20:08 -0700

    only for testing stuff

---
+++ test.bat
@@ -0,0 +1,24 @@
+@echo off
+
+ipconfig
+timeout 2 >nul
+color 2
+
+
+echo ------------------------------------------------------------
+echo .                                                         -
+echo .    WORKING       this is only made for testing stuff    -
+echo .                                                         -
+echo .         this is a testing bat file                      -
+echo .                                                         -
+echo ------------------------------------------------------------
+
+
+
+                                
+
+pause 
+
+timeout 8 >nul 
+
+pause
